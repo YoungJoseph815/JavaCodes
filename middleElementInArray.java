48f7aa2b68f66c0f7031fa2ec86bde4278b89a58
Aihemaitijiang Yusufu <49492108+AihemaitijiangYusufu@users.noreply.github.com>
2019-12-19 11:13:58 -0500

    Update middleElementInArray.java

--- middleElementInArray.java
+++ middleElementInArray.java
@@ -4,29 +4,40 @@ import java.lang.reflect.Array;
 
 public class middleElementInArray {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+		
+		int[] arr1= {1,5,8,3,9};
+		int[] arr2= {2,7,3,9,11,4,2,4};
+		
+		middleElement(arr1);
+		
+		middleElement(arr2);
 
-        String[] text={"apple","banana","grapes","orange","cucumber","potato","ahmetjan"};
+	}
+	
+	public static void middleElement(int[] arr) {
+		
+		
+		
+		int midEl;
+		
+		if(arr.length%2==0) {
+			
+			int midEl1=(arr.length/2)-1;
+			int midEl2=(arr.length/2);
+			
+			
+			
+			System.out.println(arr[midEl1]+" and "+arr[midEl2]);
+			
+		}else {
+			
+			midEl=arr.length/2;
+			
+			System.out.println(arr[midEl]);
+			
+		}
+	
+	}
 
-        arrayMiddleElement(text);
-
-
-    }
-
-    public static void arrayMiddleElement(String[] str){
-
-        int length=str.length;
-
-        if (length%2==0){
-
-            System.out.println("The array has even elements, so no middle element");
-        }else {
-
-            String middleElement=str[length/2];
-
-            System.out.println("The middle element is "+middleElement);
-
-        }
-
-    }
 }
