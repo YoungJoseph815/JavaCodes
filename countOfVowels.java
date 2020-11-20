af659256746205deff3bf84e00720287c5458d93
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2020-11-19 20:41:14 -0500

    Update countOfVowels.java

--- countOfVowels.java
+++ countOfVowels.java
@@ -45,4 +45,37 @@ public class countOfVowels {
 
 
     }
+    
+    
+      @Test
+    public void test(){
+        System.out.println(count("hello"));
+    }
+    public  String count(String word){
+        int countVowels=0;
+        int countConsonants=0;
+
+        List<Character> vowels= Arrays.asList('a','e','i','o','u');
+        List<Character> consonant= new ArrayList<>();
+
+        for(char i='a';i<='z';i++){
+            if(!vowels.contains(i)){
+                consonant.add(i);
+            }
+            word=word.toLowerCase();
+        }
+
+        for(int i=0;i<word.length();i++){
+            if(vowels.contains(word.charAt(i))){
+                countVowels++;
+            }else{
+                countConsonants++;
+            }
+        }
+        
+        String vowelsMsg="number of vowels is: "+countVowels;
+       // String consonantMsg="number of consonant is "+ consonant;
+
+        return vowelsMsg;
+    }
 }
