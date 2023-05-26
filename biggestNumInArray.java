7523b47ef3272be382c2c16bcee0d24599c65f1f
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-26 08:51:21 -0400

    Update biggestNumInArray.java

--- biggestNumInArray.java
+++ biggestNumInArray.java
@@ -7,6 +7,8 @@ public class biggestNumInArray {
         int[] num={2,4,5,78,3,2,5,9,12,32};
 
         System.out.println("The biggest number is: "+biggestNum(num));
+        System.out.println("The biggest number is: "+secondBig(num));
+        System.out.println("The biggest number is: "+ssmall(num));
 
     }
 
@@ -33,14 +35,21 @@ public class biggestNumInArray {
     
      public static int secondBig(int[] arr){
 
-        int sbig=arr[0];
-
-        for (int i=0; i<arr.length;i++){
-            if (arr[i]>sbig){
-                sbig=arr[i-1];
-            }
-        }
-        return sbig;
+      int temp;  
+      for (int i = 0; i < total; i++)   
+        {  
+            for (int j = i + 1; j < total; j++)   
+            {  
+                if (a[i] > a[j])   
+                {  
+                    temp = a[i];  
+                    a[i] = a[j];  
+                    a[j] = temp;  
+                }  
+            }  
+        }  
+       return a[total-2];  
+}  
     }
 
     public static int ssmall(int[] arr){
