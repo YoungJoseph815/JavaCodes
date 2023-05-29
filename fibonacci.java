3ad5454f340cccea535c9b07ef1808986fd71c16
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-29 09:46:48 -0400

    Update fibonacci.java
    fibonaci

--- fibonacci.java
+++ fibonacci.java
@@ -1,40 +1,23 @@
 package practice;
 
-public class fibonacci {
+public class Fibonacci {
 
-
-
-    static int num1=0, num2=1, num3;
-
-    public static void main(String[] args) {
-
-        int until = 10;
-
-        System.out.print(num1+ " "+num2);
-        fibonacci(until-2);
-
-
-
-
+public static void main(String[] args) {
+        Fibo(8);
     }
 
-    public static void fibonacci(int count) {
-
-
-        if (count>0){
-
-            num3=num1+num2;
+    static void Fibo(int Num){
+        int num1=0,num2=1,count=0;
+        while(count<Num){
+            System.out.print(num1+ " ");
+            int num3= num1+num2;
             num1=num2;
             num2=num3;
-
-            System.out.print(" "+num3);
-
-            fibonacci(count-1);
+            count+=1;
         }
-
-
-
-
     }
 
 }
+
+//#######Output####
+// 0 1 1 2 3 5 8 13 
