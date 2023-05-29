af2b8ea98d11eb199e63a2311a4accf54d4c20ae
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-29 10:08:41 -0400

    java

--- middleElementInArray.java
+++ middleElementInArray.java
@@ -4,40 +4,24 @@ import java.lang.reflect.Array;
 
 public class middleElementInArray {
 
-   public static void main(String[] args) {
-		
-		int[] arr1= {1,5,8,3,9};
-		int[] arr2= {2,7,3,9,11,4,2,4};
-		
-		middleElement(arr1);
-		
-		middleElement(arr2);
+      public static void main(String[] args) {
 
-	}
-	
-	public static void middleElement(int[] arr) {
-		
-		
-		
-		int midEl;
-		
-		if(arr.length%2==0) {
-			
-			int midEl1=(arr.length/2)-1;
-			int midEl2=(arr.length/2);
-			
-			
-			
-			System.out.println(arr[midEl1]+" and "+arr[midEl2]);
-			
-		}else {
-			
-			midEl=arr.length/2;
-			
-			System.out.println(arr[midEl]);
-			
-		}
-	
-	}
+             mid(new int[]{1,5,8,3,9});
+             mid(new int[]{2,7,3,9,11,4,2,4});
+
+    }
+
+    static void mid(int[] arr){
+        if (arr.length%2==0){
+            System.out.println(arr[(arr.length/2)-1]+" and "+arr[arr.length/2]);
+        }else {
+            System.out.println(arr[arr.length/2]);
+        }
+    }
+
 
 }
+
+// ******Output*******
+// 8
+// 9 and 11
