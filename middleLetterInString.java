679ae3b245909db1889af9a4f255b94f1722b2e5
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-29 10:14:07 -0400

    java

--- middleLetterInString.java
+++ middleLetterInString.java
@@ -2,34 +2,24 @@ package practice;
 
 public class middleLetterInString {
 	
-	public static void main(String[] args) {
-		
-		String name="ahmetjan";
-		
-		middleLetter(name);
-		
-	}
-	
-	
-	public static void middleLetter(String str) {
-		
-		char[] letter=str.toCharArray();
-		
-		if(letter.length%2==0) {
-			
-			int midL1=(letter.length/2)-1;
-			int midL2=letter.length/2;
-			
-			System.out.println(letter[midL1]+ " and "+letter[midL2]);
-		}else {
-			
-			int midL=letter.length/2;
-			
-			System.out.println(letter[midL]);
-			
-		}
-		
-		
-	}
+	 public static void main(String[] args) {
+
+        midStr("Odilaq");
+        midStr("Budilaq");
+    }
+
+    static void midStr(String str){
+        char[] letter=str.toCharArray();
+        if (letter.length%2==0){
+            System.out.println(letter[(letter.length/2)-1]+" and "+letter[letter.length/2]);
+        }else {
+            System.out.println(letter[letter.length/2]);
+        }
+    }
 
 }
+
+
+//******Output****
+//i and l
+//i
