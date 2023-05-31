f0820c3bd885abc24c409de83d938e9fe7b561cb
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-30 20:50:00 -0400

    Update PrimeNumber

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,30 +2,28 @@ package practice;
 
 public class PrimeNumber {
 
-    public static void main(String[] args) {
-
-        int san=4;
-
-        System.out.println(isPrimeNumber(san));
-
+        public static void main(String[] args) {
+        System.out.println(isPrime(0));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(4));
+        System.out.println(isPrime(7));
+        System.out.println(isPrime(9));
     }
-
-    public static boolean isPrimeNumber(int num){
-
+    static boolean isPrime(int num){
         if (num<=1){
-
             return false;
         }
-
-
-        for (int i=2; i<num; i++){
-
+        for (int i=2;i<num;i++){
             if (num%i==0){
-
                 return false;
             }
         }
-
-             return true;
+        return true;
     }
 }
+
+//*********Output*******
+//        false
+//        false
+//        true
+//        false
