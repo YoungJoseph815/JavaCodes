d15ce00c9c2cd1a8b4be0c71c6aa7f428d4b8a27
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-30 20:17:04 -0400

    Update countingFrequencyOfNum

--- countingFrequencyOfNum.java
+++ countingFrequencyOfNum.java
@@ -4,50 +4,44 @@ import java.util.Arrays;
 
 public class countingFrequencyOfNum {
 
-
     public static void main(String[] args) {
-
-        int[] num={4,3,8,5,1,4,3,8,7,7,6,5,9};
-
-        frequancy(num);
-
-
-
+        frequency(new int[]{1,1,2,1,4,32,32,1,0,67,5,6,6,2});
     }
 
-    public static void frequancy(int[] arr){
-
-
-        int temp=0;
-        int[]  count = new int[arr.length];
-
-
-        for (int i=0; i<arr.length; i++){
-
-            if (arr[i]==0){
-
-                break;
-
-            }else {
-
-                temp=arr[i];
-                count[temp]++;
-
+    static void frequency(int[] arr){
+        for (int i=0;i<arr.length;i++){
+            int flag=0;
+            int count=0;
+            for (int j=i+1;j<=i;j++){
+                if (arr[i]==arr[j]){
+                    flag=1;
+                    break;
+                }
             }
-        }
-
-
-        for (int j=0; j<count.length;j++){
-
-            if (count[j]==1){
-
-                System.out.println(j+" occures "+count[j]+" time.");
-            }else if(count[j]>1){
-
-                System.out.println(j+" occures "+count[j]+" times.");
+            if (flag==1)
+                continue;
+            for (int j=0;j<arr.length;j++){
+                if (arr[i]==arr[j]){
+                    count++;
+                }
             }
+            System.out.println(arr[i]+" occures "+count+" time/s.");
         }
-
     }
-
 }
+
+//*********Output********
+//        1 occures 4 time/s.
+//        1 occures 4 time/s.
+//        2 occures 2 time/s.
+//        1 occures 4 time/s.
+//        4 occures 1 time/s.
+//        32 occures 2 time/s.
+//        32 occures 2 time/s.
+//        1 occures 4 time/s.
+//        0 occures 1 time/s.
+//        67 occures 1 time/s.
+//        5 occures 1 time/s.
+//        6 occures 2 time/s.
+//        6 occures 2 time/s.
+//        2 occures 2 time/s.
