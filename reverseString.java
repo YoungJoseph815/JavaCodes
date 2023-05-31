18fdf4134b23b26da58ab2232705dbfb55be0802
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-31 11:28:15 -0400

    Update reverseString

--- reverseString.java
+++ reverseString.java
@@ -2,26 +2,17 @@ package practice;
 
 public class reverseString {
 
-    public static void main(String[] args) {
-
-
-        String word="ahmetjan";
-
-        System.out.println(reverseString(word));
-
+     public static void main(String[] args) {
+        System.out.println(reverse("ahmetjan"));
     }
-
-    public static String reverseString(String str){
-
+    static String reverse(String str){
         String reverse="";
-
         for (int i=str.length()-1;i>=0;i--){
-
             reverse+=str.charAt(i);
         }
-
         return reverse;
-
-
     }
 }
+
+//********Output***
+//najtemha
