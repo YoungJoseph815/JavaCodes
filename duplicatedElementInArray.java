bfa6651b4259b69b64da463fa99986750030fc27
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-31 08:19:40 -0400

    Update duplicatedElementInArray

--- duplicatedElementInArray.java
+++ duplicatedElementInArray.java
@@ -2,35 +2,21 @@ package practice;
 
 public class duplicatedElementInArray {
 
-    public static void main(String[] args) {
-
-        int[] san={1,2,4,3,2,1,6,7,8,8,6,5,4,5,3,2,3,1};
-
-        duplicateChecker(san);
-
-
-
+     public static void main(String[] args) {
+       dupe(new int[]{1,1,2,3,3,4,5,6});
     }
-
-    public static void duplicateChecker(int[] num){
-
-        for (int i=0; i<num.length;i++){
-
-
-
-            for (int j=i+1; j<num.length;j++){
-
-                if (num[i]==num[j]){
-
-
-                    System.out.println(num[i]+" is duplicated ");
-
+    static void dupe(int[] arr){
+        Set<Integer> set=new HashSet<>();
+        for (int i=0;i<arr.length;i++){
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[i]==arr[j]){
+                    set.add(arr[i]);
                 }
-
-
             }
-
         }
+        System.out.println("Dupelicated elements are: "+set);
     }
-
 }
+
+//***********Outpput******
+//Dupelicated elements are: [1, 3]
