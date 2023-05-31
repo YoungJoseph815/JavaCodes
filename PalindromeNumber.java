9d3f858b9810e610c7ff90c09970e14ebb2d9792
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-30 21:00:00 -0400

    Update PalindromeNumber

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -3,42 +3,25 @@ package practice;
 public class PalindromeNumber {
 
     public static void main(String[] args) {
-
-        int san=123321;
-
-        System.out.println(isPalindrome(san));
-
-
+        System.out.println(isPalindrome(12121));
+        System.out.println(isPalindrome(10000));
     }
-
-    public static boolean isPalindrome(int num){
-
-        int original,remainder=0,reverse=0;
-
+    static boolean isPalindrome(int num){
+        int original,reverse=0,remainder=0;
         original=num;
-
-
-        while (original!=0){
-
-            remainder=original%10;
-            reverse=reverse*10+remainder;
-            original=original/10;
-
-        }
-
-
-        if (num==reverse){
-
-            return true;
-        }else{
-
-            return false;
-        }
-
-
-
-
-
-
+       while (original!=0){
+           remainder=original%10;
+           reverse=reverse*10+remainder;
+           original=original/10;
+       }
+       if (num==reverse){
+           return true;
+       }else {
+           return false;
+       }
     }
 }
+
+//*********Output****8
+//true
+//false
