3e9628269e014ae6a74572957cea4c6a5ef5d15c
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-31 11:56:15 -0400

    Update starNameRepeating

--- starNameRepeating.java
+++ starNameRepeating.java
@@ -2,30 +2,22 @@ package practice;
 
 public class starNameRepeating {
 
-    public static void main(String[] args) {
-
-
-        String word="*";
-        String name="Ahmetjan";
-
-        nameRepeating(word);
-        nameRepeating(name);
-
-
+   public static void main(String[] args) {
+        star("hhh",5);
     }
-
-    public static void nameRepeating(String str){
-
-
-        for (int i=0;i<5;i++){
-
-            for (int j=0; j<=i;j++){
-
+    static void star(String str,int repeatCount){
+        for (int i=0;i<repeatCount;i++){
+            for (int j=0;j<=i;j++){
                 System.out.print(str+" ");
             }
-
-                System.out.println();
+            System.out.println();
         }
     }
-
 }
+
+//********Output***
+//        hhh
+//        hhh hhh
+//        hhh hhh hhh
+//        hhh hhh hhh hhh
+//        hhh hhh hhh hhh hhh 
