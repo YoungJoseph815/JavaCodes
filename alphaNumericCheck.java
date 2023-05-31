1d8d8e517573866407c043c32d72af9275badfa0
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-30 20:30:38 -0400

    Update alphaNumericCheck

--- alphaNumericCheck.java
+++ alphaNumericCheck.java
@@ -2,55 +2,28 @@ package practice;
 
 public class alphaNumericCheck {
 
-
-
-        public static void main(String...s)
-        {
-             String s1="adA12";
-             String s2="jh@l";
-             String s3="7837483";
-             String s4="jhdsbfsjhfb";
-
-            System.out.println(numericCheck(s3));
-            System.out.println(alphabeticCheck(s4));
-            System.out.println(alphaNumericCheck(s1));
-
-
+    public static void main(String[] args) {
+        alphaNumeric("343fskjfbdskj");
+        alphaNumeric("budkjsndkasj");
+        alphaNumeric("1234567");
+        alphaNumeric("budkjsndkasj$$$$");
+    }
+    static void alphaNumeric(String str){
+        if (str.matches("[0-9]+")){
+            System.out.println(str+ " is numeric!");
+        }else if (str.matches("[a-zA-Z]+")){
+            System.out.println(str+ " is alphabic!");
+        }else if (str.matches("[a-zA-Z0-9]+")){
+            System.out.println(str+ " is alphanumeric!");
+        }else {
+            System.out.println(str+ " is not numeric or alphabic nor alphanumeric!");
+        }
     }
 
-    public static boolean numericCheck(String str){
-
-            if (str.matches("[0-9]+")){
-
-                return true;
-            }else {
-
-                return false;
-            }
-    }
-
-    public static boolean alphabeticCheck(String str){
-
-            if (str.matches("[a-zA-Z]+")){
-
-                return true;
-
-            }else {
-
-                return false;
-            }
-    }
-
-    public static boolean alphaNumericCheck(String str){
-
-            if (str.matches("[a-zA-Z0-9]+")){
-
-                return true;
-            }else {
-
-                return false;
-            }
-
-
-    }
 }
+
+//***************Output********
+//        343fskjfbdskj is alphanumeric!
+//        budkjsndkasj is alphabic!
+//        1234567 is numeric!
+//        budkjsndkasj$$$$ is not numeric or alphabic nor alphanumeric!
