712417a1a4cbd7488107372e70ad05a8ddb850bb
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-31 11:46:14 -0400

    Update sortArrayList

--- sortArrayList.java
+++ sortArrayList.java
@@ -1,78 +1,32 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-public class sortArrayList {
+public class SortArrayList {
 
     public static void main(String[] args) {
-
-        List<Integer> list=new ArrayList<Integer>();
-
-        list.add(3);
+        List<Integer> list=new ArrayList<>();
+        list.add(13);
         list.add(5);
         list.add(8);
-        list.add(2);
-        list.add(9);
-        list.add(0);
-        list.add(2);
-
-
-
-//        Iterator<Integer> iterator=list.iterator();
-//
-//        while(iterator.hasNext()){
-//
-//            System.out.print(iterator.next()+" ");
-//        }
-//
-//        System.out.println();
-//
-//        for (int mylist:list){
-//
-//            System.out.print(mylist+" ");
-//        }
-//        System.out.println();
-
-//
-//        for (int i=0; i<list.size();i++){
-//
-//            System.out.print(list.get(i)+" ");
-//        }
-//
-
-        sortArrayList((ArrayList<Integer>) list);
-
-
-
-
-
-
+        System.out.println(list);
+        sortArrLst((ArrayList<Integer>)list);
     }
-
-
-   public static void sortArrayList(ArrayList<Integer> arrayList){
-
-        int temp;
-
-        for (int i=0; i<arrayList.size();i++){
-            for (int j=i+1;j<arrayList.size();j++){
-
-                if (arrayList.get(i)>arrayList.get(j)){
-
-                    temp=arrayList.get(i);
-                    arrayList.set(i,arrayList.get(j));
-                    arrayList.set(j,temp);
-
-
+    static void sortArrLst(ArrayList<Integer> arrayList){
+        for (int i=0;i<arrayList.size();i++){
+            for (int j=i+1; j<arrayList.size();j++) {
+                if (arrayList.get(i) > arrayList.get(j)) {
+                    int temp = arrayList.get(i);
+                    arrayList.set(i, arrayList.get(j));
+                    arrayList.set(j, temp);
                 }
             }
         }
-
-       System.out.println(arrayList);
-
-
-
-   }
+        System.out.println(arrayList);
+    }
 }
+
+//********Output***
+// [13, 5, 8]
+// [5, 8, 13]
