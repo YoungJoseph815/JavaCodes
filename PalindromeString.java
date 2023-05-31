243957a6d29d3b1aa3c7dd7c0e8d4edb617ec41b
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-30 20:53:30 -0400

    Update PalindromeString

--- PalindromeString.java
+++ PalindromeString.java
@@ -3,45 +3,24 @@ package practice;
 public class PalindromeString {
 
 
+    
     public static void main(String[] args) {
-
-        String word="qazaq";
-
-        System.out.println(isPalindrome(word));
-
-
+        System.out.println(isPalindrome("qazaq"));
+        System.out.println(isPalindrome("uyghur"));
     }
-
-    public static boolean isPalindrome(String str) {
-
-
-        String reverse ="";
-
-
-
-
-        for (int i = str.length()-1; i >=0; i--) {
-
-                reverse += str.charAt(i);
-            }
-
-
-
-        if (reverse.equalsIgnoreCase(str)) {
-
-                return true;
-
-        } else {
-
-                return false;
+    static boolean isPalindrome(String str){
+        String reverse="";
+        for (int i=str.length()-1;i>=0;i--){
+            reverse+=str.charAt(i);
         }
-
-
-
-
+        if (reverse.equalsIgnoreCase(str)){
+            return true;
+        }else {
+            return false;
+        }
     }
-
-
-
-
 }
+
+//********Output****
+// true
+// false
