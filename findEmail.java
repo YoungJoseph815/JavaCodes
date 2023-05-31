fe393ed5d8b10de5ee7d0cdaba394cc857e82f10
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-05-31 08:28:22 -0400

    Update findEmail

--- findEmail.java
+++ findEmail.java
@@ -5,22 +5,19 @@ import java.util.regex.Pattern;
 
 public class findEmail {
 
-    public static void main(String[] args) {
-
-        String text="Welcome to use those email address: bhdbf12@gmail.com, jn7dfd@hotmail.com,dnjdc@ahmatmail.com Thank you.";
-
-        emailFinder(text);
-
+       public static void main(String[] args) {
+         emailFinder("Welcome to use those email address: bhdbf12@gmail.com, jn7dfd@hotmail.com,dnjdc@ahmatmail.com Thank you.");
     }
-
-    public static void emailFinder(String str){
-
-        Pattern pattern=Pattern.compile("([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+[a-z])");
+    static void emailFinder(String str){
+        Pattern pattern=Pattern.compile("([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+[a-z)])");
         Matcher matcher=pattern.matcher(str);
-
-        while(matcher.find()){
+        while (matcher.find()){
             System.out.println(matcher.group());
         }
-
     }
 }
+
+//***********Outpput******
+//bhdbf12@gmail.com
+//jn7dfd@hotmail.com
+//dnjdc@ahmatmail.com
