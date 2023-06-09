30acd1c28b05b75528d1a694ae5bc71b2b8e5f01
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2023-06-09 12:19:59 -0400

    ☕GeneticMethod.java

---
+++ ☕GeneticMethod.java
@@ -0,0 +1,28 @@
+public class GeneticMethod {
+  
+  //Generic allows similar data types to be passed as parameters.
+
+    public static void main(String[] args) {
+
+        Integer[] num=new Integer[]{1,2,3,4,5};
+        String[] str=new String[]{"Hello","World"};
+        Character[] ch=new Character[]{'a','u','i','o','e'};
+
+        getArrayElement(num);
+        getArrayElement(str);
+        getArrayElement(ch);
+
+    }
+    static <E> void getArrayElement(E[] array){
+         for (E element: array){
+             System.out.print(element+" ");
+         }
+             System.out.println();
+    }
+}
+
+//*******Output*****
+// 1 2 3 4 5 
+// Hello World 
+// a u i o e 
+
