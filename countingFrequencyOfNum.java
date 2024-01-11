c2f9ef5d1239849629ac20c36a492aed4de0a3f2
Young Joseph <49492108+YoungJoseph815@users.noreply.github.com>
2024-01-11 05:22:06 -0500

    Update countingFrequencyOfNum.java

--- countingFrequencyOfNum.java
+++ countingFrequencyOfNum.java
@@ -9,39 +9,24 @@ public class countingFrequencyOfNum {
     }
 
     static void frequency(int[] arr){
-        for (int i=0;i<arr.length;i++){
-            int flag=0;
-            int count=0;
-            for (int j=i+1;j<=i;j++){
-                if (arr[i]==arr[j]){
-                    flag=1;
-                    break;
-                }
-            }
-            if (flag==1)
-                continue;
-            for (int j=0;j<arr.length;j++){
-                if (arr[i]==arr[j]){
-                    count++;
-                }
-            }
-            System.out.println(arr[i]+" occures "+count+" time/s.");
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int element:
+             arr) {
+            map.put(element,map.getOrDefault(element,0)+1);
         }
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()){
+            System.out.println("Element: "+entry.getKey()+" \t "+"Frequency: "+entry.getValue());
+        }
+
     }
 }
 
 //*********Output********
-//        1 occures 4 time/s.
-//        1 occures 4 time/s.
-//        2 occures 2 time/s.
-//        1 occures 4 time/s.
-//        4 occures 1 time/s.
-//        32 occures 2 time/s.
-//        32 occures 2 time/s.
-//        1 occures 4 time/s.
-//        0 occures 1 time/s.
-//        67 occures 1 time/s.
-//        5 occures 1 time/s.
-//        6 occures 2 time/s.
-//        6 occures 2 time/s.
-//        2 occures 2 time/s.
+// Element: 32 	 Frequency: 2
+// Element: 0 	 Frequency: 1
+// Element: 1 	 Frequency: 4
+// Element: 2 	 Frequency: 2
+// Element: 67 	 Frequency: 1
+// Element: 4 	 Frequency: 1
+// Element: 5 	 Frequency: 1
+// Element: 6 	 Frequency: 2
